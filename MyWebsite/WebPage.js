f9304619fca44c43d44fdb107a7f728d9134969d
parkjaeyi <113220707+parkjaeyi@users.noreply.github.com>
2023-09-26 23:22:07 -0400

    color task update

--- MyWebsite/WebPage.js
+++ MyWebsite/WebPage.js
@@ -27,7 +27,7 @@ window.addEventListener('load',()=>{
 
         if(!valuedate){
             const task_el=document.createElement("div");
-            task_el.classList.add('task');
+            task_el.classList.add('task1');
 
 
 
@@ -63,6 +63,16 @@ window.addEventListener('load',()=>{
             // task_date_el.classList.add('date');
             // task_date_el.type='date';
 
+            const color_task_el=document.createElement('input');
+            color_task_el.classList.add('T1colourbutton');
+            color_task_el.type='button';
+            const color2_task_el=document.createElement('input');
+            color2_task_el.classList.add('T1colourbutton2');
+            color2_task_el.type='button';
+            const color3_task_el=document.createElement('input');
+            color3_task_el.classList.add('T1colourbutton3');
+            color3_task_el.type='button';
+
             const task_edit_el=document.createElement("image");
             task_edit_el.classList.add('edit');
             task_edit_el.innerHTML= "<input type='image' id='edit' src='pen-to-square-regular.svg' alt='edit' height='30'/></i>";
@@ -75,7 +85,9 @@ window.addEventListener('load',()=>{
             task_delete_el.classList.add('X');
             task_delete_el.innerHTML="<input type='image' id='X' src='eraser-solid.svg' alt='delete' height='30'/></i>"
 
-            //task_actions_el.appendChild(task_date_el);
+            task_actions_el.appendChild(color_task_el);
+            task_actions_el.appendChild(color2_task_el);
+            task_actions_el.appendChild(color3_task_el);
             task_actions_el.appendChild(task_edit_el);
             task_actions_el.appendChild(task_delete_el);
             
@@ -83,13 +95,125 @@ window.addEventListener('load',()=>{
             
 
             task_el.appendChild(task_actions_el);
-            //og_task.appendChild(task_el);
             list_el.appendChild(task_el);
 
             input.value='';// it will reset the textbox
             
             
     //MAKING BUTTONS TO WORK
+            //button red
+            
+            color_task_el.addEventListener('click',()=>{
+               if(task_el.style.backgroundColor='red'){
+                    task_el.style.background='none';
+                }
+
+                colorRed();
+
+            })
+            function colorRed(){
+                task_el.style.backgroundColor='red';
+                task_input_el.style.color='white';
+
+                color_task_el.addEventListener('click',()=>{
+                   button1();
+                   return;
+                })
+                // return task_el.style.backgroundColor='red',task_input_el.style.color='white';
+                
+            }
+           
+            function button1(){
+                if(task_el.style.backgroundColor='red'){
+                    task_el.style.background='none';
+                    task_input_el.style.color='grey';
+                    
+                }
+                
+                color_task_el.addEventListener('click',()=>{
+                    colorRed();
+                   
+                })
+                color2_task_el.addEventListener('click',()=>{
+                    colorOrange();
+                    
+                })
+                color3_task_el.addEventListener('click',()=>{
+                     colorAqua();
+                     
+                })
+                
+            }
+            //button orange
+            color2_task_el.addEventListener('click',()=>{
+                if(task_el.style.backgroundColor='orange'){
+                    task_el.style.background='none';
+                }
+                colorOrange();
+            })
+            function colorOrange(){
+                task_el.style.backgroundColor='orange';
+                task_input_el.style.color='black';
+                color2_task_el.addEventListener('click',()=>{
+                   button2();
+                })
+                
+            }
+           
+            function button2(){
+                if(task_el.style.backgroundColor='orange'){
+                    task_el.style.background='none';
+                    task_input_el.style.color='grey';
+                }
+                color_task_el.addEventListener('click',()=>{
+                    colorRed();
+                    return;
+                })
+                color2_task_el.addEventListener('click',()=>{
+                    colorOrange();
+                    return;
+                })
+                color3_task_el.addEventListener('click',()=>{
+                    colorAqua();
+                    return;
+                })
+            }
+            //button aqua
+            color3_task_el.addEventListener('click',()=>{
+                if(task_el.style.backgroundColor='aqua'){
+                    task_el.style.background='none';
+                }
+                colorAqua();
+            })
+            function colorAqua(){
+                task_el.style.backgroundColor='aqua';
+                task_input_el.style.color='chocolate';
+                color3_task_el.addEventListener('click',()=>{
+                   button3();
+                })
+            }
+           
+            function button3(){
+                if(task_el.style.backgroundColor='aqua'){
+                    task_el.style.background='none';
+                    task_input_el.style.color='grey';
+                }
+                color_task_el.addEventListener('click',()=>{
+                    colorRed();
+                    return;
+                })
+                color2_task_el.addEventListener('click',()=>{
+                    colorOrange();
+                    return;
+                })
+                color3_task_el.addEventListener('click',()=>{
+                    colorAqua();
+                    return;
+                })
+                    
+           
+            }
+            //edit button
             task_edit_el.addEventListener('click',()=>{
                 taskEditFunction();
             })
@@ -147,7 +271,9 @@ window.addEventListener('load',()=>{
                 crossoutSound('pencil_check_mark_1-88805-[AudioTrimmer.com].mp3');
                 task_input_el.style.textDecoration="line-through";
                 task_actions_el.removeChild(task_edit_el);
-                //task_actions_el.removeChild(task_date_el);
+                task_actions_el.removeChild(color_task_el);
+                task_actions_el.removeChild(color2_task_el);
+                task_actions_el.removeChild(color3_task_el);
                 task_el.appendChild(task_actions_el);
                 complete_el.append(task_el);
 
@@ -163,7 +289,9 @@ window.addEventListener('load',()=>{
             function undo(){
                 complete_el.removeChild(task_el);
                 task_actions_el.removeChild(task_delete_el);
-                //task_actions_el.append(task_date_el);
+                task_actions_el.append(color_task_el);
+                task_actions_el.append(color2_task_el);
+                task_actions_el.append(color3_task_el);
                 task_actions_el.append(task_edit_el);
                 task_actions_el.append(task_delete_el);
                 task_el.appendChild(task_actions_el);
@@ -187,17 +315,11 @@ window.addEventListener('load',()=>{
         }//end of if statement
         else{
             const task_el=document.createElement("div");
-        task_el.classList.add('task');
-
-
-
-        // const og_task=document.createElement('div');
-        // og_task.classList.add('task');
-        
+            task_el.classList.add('task');
 
         const task_content_el=document.createElement("div");
         task_content_el.classList.add('content');
-        //task_content_el.innerText=task;
+   
 
         const listItem=document.createElement('li');
         listItem.classList.add('content');
@@ -205,7 +327,7 @@ window.addEventListener('load',()=>{
        
         task_el.appendChild(listItem);
         task_el.appendChild(task_content_el);
-        //og_task.appendChild(task_el);
+        
 
         const task_input_el=document.createElement('input');
         task_input_el.classList.add("text"); //add the class text
@@ -218,6 +340,16 @@ window.addEventListener('load',()=>{
 
         const task_actions_el=document.createElement('div');
         task_actions_el.classList.add('action');
+
+        const color_task_el=document.createElement('input');
+        color_task_el.classList.add('colourbutton');
+        color_task_el.type='button';
+        const color2_task_el=document.createElement('input');
+        color2_task_el.classList.add('colourbutton2');
+        color2_task_el.type='button';
+        const color3_task_el=document.createElement('input');
+        color3_task_el.classList.add('colourbutton3');
+        color3_task_el.type='button';
 
         const task_date_el=document.createElement("input");
         task_date_el.classList.add('date');
@@ -237,6 +369,9 @@ window.addEventListener('load',()=>{
         task_delete_el.innerHTML="<input type='image' id='X' src='eraser-solid.svg' alt='delete' height='30'/></i>"
 
         task_actions_el.appendChild(task_date_el);
+        task_actions_el.appendChild(color_task_el);
+        task_actions_el.appendChild(color2_task_el);
+        task_actions_el.appendChild(color3_task_el);
         task_actions_el.appendChild(task_edit_el);
         task_actions_el.appendChild(task_delete_el);
         
@@ -246,12 +381,125 @@ window.addEventListener('load',()=>{
         task_el.appendChild(task_actions_el);
         //og_task.appendChild(task_el);
         list_el.appendChild(task_el);
-
+        
+        
         input.value='';// it will reset the textbox
         document.getElementById('date-input').value='';//resets the date
         
         
 //MAKING BUTTONS TO WORK
+        //button red
+        color_task_el.addEventListener('click',()=>{
+            if(task_el.style.backgroundColor='red'){
+                 task_el.style.background='none';
+             }
+
+             colorRed();
+
+         })
+         function colorRed(){
+             task_el.style.backgroundColor='red';
+             task_input_el.style.color='white';
+
+             color_task_el.addEventListener('click',()=>{
+                button1();
+                return;
+             })
+             // return task_el.style.backgroundColor='red',task_input_el.style.color='white';
+             
+         }
+        
+         function button1(){
+             if(task_el.style.backgroundColor='red'){
+                 task_el.style.background='none';
+                 task_input_el.style.color='grey';
+                 
+             }
+             
+             color_task_el.addEventListener('click',()=>{
+                 colorRed();
+                
+             })
+             color2_task_el.addEventListener('click',()=>{
+                 colorOrange();
+                 
+             })
+             color3_task_el.addEventListener('click',()=>{
+                  colorAqua();
+                  
+             })
+             
+         }
+         //button orange
+         color2_task_el.addEventListener('click',()=>{
+             if(task_el.style.backgroundColor='orange'){
+                 task_el.style.background='none';
+             }
+             colorOrange();
+         })
+         function colorOrange(){
+             task_el.style.backgroundColor='orange';
+             task_input_el.style.color='black';
+             color2_task_el.addEventListener('click',()=>{
+                button2();
+             })
+             
+         }
+        
+         function button2(){
+             if(task_el.style.backgroundColor='orange'){
+                 task_el.style.background='none';
+                 task_input_el.style.color='grey';
+             }
+             color_task_el.addEventListener('click',()=>{
+                 colorRed();
+                 return;
+             })
+             color2_task_el.addEventListener('click',()=>{
+                 colorOrange();
+                 return;
+             })
+             color3_task_el.addEventListener('click',()=>{
+                 colorAqua();
+                 return;
+             })
+         }
+         //button aqua
+         color3_task_el.addEventListener('click',()=>{
+             if(task_el.style.backgroundColor='aqua'){
+                 task_el.style.background='none';
+             }
+             colorAqua();
+         })
+         function colorAqua(){
+             task_el.style.backgroundColor='aqua';
+             task_input_el.style.color='chocolate';
+             color3_task_el.addEventListener('click',()=>{
+                button3();
+             })
+         }
+        
+         function button3(){
+             if(task_el.style.backgroundColor='aqua'){
+                 task_el.style.background='none';
+                 task_input_el.style.color='grey';
+             }
+             color_task_el.addEventListener('click',()=>{
+                 colorRed();
+                 return;
+             })
+             color2_task_el.addEventListener('click',()=>{
+                 colorOrange();
+                 return;
+             })
+             color3_task_el.addEventListener('click',()=>{
+                 colorAqua();
+                 return;
+             })
+                 
+        
+         }
+        //edit button
         task_edit_el.addEventListener('click',()=>{
             taskEditFunction();
         })
@@ -304,11 +552,14 @@ window.addEventListener('load',()=>{
             }
 
         })
-
+    
         function crossout(){
             crossoutSound('pencil_check_mark_1-88805-[AudioTrimmer.com].mp3');
             task_input_el.style.textDecoration="line-through";
             task_actions_el.removeChild(task_edit_el);
+            task_actions_el.removeChild(color_task_el);
+            task_actions_el.removeChild(color2_task_el);
+            task_actions_el.removeChild(color3_task_el);
             task_actions_el.removeChild(task_date_el);
             task_el.appendChild(task_actions_el);
             complete_el.append(task_el);
@@ -326,6 +577,9 @@ window.addEventListener('load',()=>{
             complete_el.removeChild(task_el);
             task_actions_el.removeChild(task_delete_el);
             task_actions_el.append(task_date_el);
+            task_actions_el.append(color_task_el);
+            task_actions_el.append(color2_task_el);
+            task_actions_el.append(color3_task_el);
             task_actions_el.append(task_edit_el);
             task_actions_el.append(task_delete_el);
             task_el.appendChild(task_actions_el);
