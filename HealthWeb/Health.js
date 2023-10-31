c8e451b563fd011ee70549a0be2842562681b824
parkjaeyi <113220707+parkjaeyi@users.noreply.github.com>
2023-10-31 16:37:18 -0400

    js update

---
+++ HealthWeb/Health.js
@@ -0,0 +1,10 @@
+const nextButton=document.getElementById("next");
+const prevButton=document.getElementById("prev");
+let HealthPages=[document.getElementById("Homepage"), document.getElementById("Secondpage")];
+let pages=0;
+nextButton.addEventListener("click",()=>{
+//    location.href='HealthP2.html';
+});
+prevButton.addEventListener("click",()=>{
+    // location.href='Health.html';
+ });
