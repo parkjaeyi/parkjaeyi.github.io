1795facd7128a2d1a86cd9da286b36660fa74385
parkjaeyi <113220707+parkjaeyi@users.noreply.github.com>
2023-10-31 17:04:51 -0400

    next/prev button

--- HealthWeb/Health.js
+++ HealthWeb/Health.js
@@ -1,10 +1,11 @@
-const nextButton=document.getElementById("next");
-const prevButton=document.getElementById("prev");
-let HealthPages=[document.getElementById("Homepage"), document.getElementById("Secondpage")];
-let pages=0;
-nextButton.addEventListener("click",()=>{
-//    location.href='HealthP2.html';
-});
-prevButton.addEventListener("click",()=>{
-    // location.href='Health.html';
- });
+// const nextButton=document.getElementById("next");
+// const prevButton=document.getElementById("prev");
+// let HealthPages=[document.getElementById("Homepage"), document.getElementById("Secondpage")];
+// let pages=0;
+// nextButton.addEventListener("click",()=>{
+//      location.href='HealthP2.html';
+    
+// });
+// prevButton.addEventListener("click",()=>{
+//      location.href='Health.html';
+//  });
