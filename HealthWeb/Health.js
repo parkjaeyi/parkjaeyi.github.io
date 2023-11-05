d5c4db9a7d6b9dc4e580e6c631dcdacfbc93cbed
parkjaeyi <113220707+parkjaeyi@users.noreply.github.com>
2023-11-04 21:12:29 -0400

    Add files via upload

--- HealthWeb/Health.js
+++ HealthWeb/Health.js
@@ -1,11 +1,18 @@
-// const nextButton=document.getElementById("next");
-// const prevButton=document.getElementById("prev");
+const HomeButton=document.getElementById("Home");
+const TrackerButton=document.getElementById("Tracker");
+const ExerciseButton=document.getElementById("Exercise");
+const AboutButton=document.getElementById("AboutUs");
 // let HealthPages=[document.getElementById("Homepage"), document.getElementById("Secondpage")];
 // let pages=0;
-// nextButton.addEventListener("click",()=>{
-//      location.href='HealthP2.html';
-    
-// });
-// prevButton.addEventListener("click",()=>{
-//      location.href='Health.html';
-//  });
+HomeButton.addEventListener("click",()=>{
+    location.href='Health.html';
+});
+TrackerButton.addEventListener("click",()=>{
+    location.href='HealthP2.html';
+ });
+ExerciseButton.addEventListener("click",()=>{
+    location.href='HealthP3.html';
+ });
+AboutButton.addEventListener("click",()=>{
+    location.href='HealthP4.html';
+ });
