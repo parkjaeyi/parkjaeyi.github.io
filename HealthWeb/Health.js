ffb2abe810c7d3ec1ada303c00b66d32fda0c689
parkjaeyi <113220707+parkjaeyi@users.noreply.github.com>
2023-11-05 17:24:09 -0500

    Update Health.js

--- HealthWeb/Health.js
+++ HealthWeb/Health.js
@@ -1,18 +1,14 @@
-const HomeButton=document.getElementById("Home");
-const TrackerButton=document.getElementById("Tracker");
-const ExerciseButton=document.getElementById("Exercise");
-const AboutButton=document.getElementById("AboutUs");
-// let HealthPages=[document.getElementById("Homepage"), document.getElementById("Secondpage")];
-// let pages=0;
-HomeButton.addEventListener("click",()=>{
-    location.href='Health.html';
-});
-TrackerButton.addEventListener("click",()=>{
-    location.href='HealthP2.html';
- });
-ExerciseButton.addEventListener("click",()=>{
-    location.href='HealthP3.html';
- });
-AboutButton.addEventListener("click",()=>{
-    location.href='HealthP4.html';
- });
+"use strict";
+
+//Menu toggle
+
+var navLinks=document.getElementById("nav-links");
+
+function showMenu(){
+    navLinks.style.display="block";
+    navLinks.style.right="0";
+}
+
+function hideMenu(){
+    navLinks.style.display="none";
+}
