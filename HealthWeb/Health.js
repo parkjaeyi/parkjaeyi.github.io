a8d493ff8871cadf1db268a565b73c98cc581952
parkjaeyi <parkjaeyi@gmail.com>
2023-11-13 09:02:09 -0500

    testing

--- HealthWeb/Health.js
+++ HealthWeb/Health.js
@@ -18,3 +18,26 @@ document.getElementById("signIn").addEventListener("click",()=>{
 document.querySelector(".close").addEventListener("click",()=>{
     document.querySelector(".popup").style.display="none";
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const areas = document.querySelectorAll('map area');
+
+    areas.forEach(area => {
+        area.addEventListener('click', () => {
+            const sectionId = area.getAttribute('href').substring(1); 
+            highlightSection(sectionId);
+        });
+    });
+
+    function highlightSection(sectionId) {
+        const sections = document.querySelectorAll('section');
+        sections.forEach(section => {
+            section.classList.remove('highlighted');
+        });
+
+        const selectedSection = document.getElementById(sectionId);
+        if (selectedSection) {
+            selectedSection.classList.add('highlighted');
+        }
+    }
+});
