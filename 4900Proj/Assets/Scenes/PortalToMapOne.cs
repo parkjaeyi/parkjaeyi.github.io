45ff32098e25c9d55ead4091d6f4eb317b771023
parkjaeyi <113220707+parkjaeyi@users.noreply.github.com>
2024-04-17 23:13:39 -0400

    Linking the maps together
    map1 and map2 are linked together, can go back and forth now

---
+++ 4900Proj/Assets/Scenes/PortalToMapOne.cs
@@ -0,0 +1,34 @@
+//Developed by Carl Ngu
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalToMapOne : MonoBehaviour
+{
+    public BoxCollider2D portalCollider;
+    public GameObject player;
+
+    private void Awake()
+    {
+        portalCollider = GetComponent<BoxCollider2D>();
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && Vector2.Distance(player.transform.position, transform.position) < 0.5f)
+        {
+            portalCollider.enabled = true;
+        }
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
